d16ec1388064e3a1ded40c5765e2c95fc340ea91
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-12-25 10:35:30 -0800

    Adding Navigation (Closes #6)

--- index.android.js
+++ index.android.js
@@ -9,13 +9,42 @@ var styles = ReactNative.StyleSheet.create({
     backgroundColor: 'white',
     fontSize: 30,
     margin: 80
+  },
+  container: {
+    flex: 1
   }
 });
 
-class tutorialReactnativeWenderlich extends React.Component {
+class HelloWorld extends React.Component {
   render() {
-    return <ReactNative.Text style={styles.text}>Hello World (Again)</ReactNative.Text>;
+    return (
+      <ReactNative.Text style={styles.text}>Hello World (Again)</ReactNative.Text>
+    );
   }
 }
 
-ReactNative.AppRegistry.registerComponent('tutorialReactnativeWenderlich', function() { return tutorialReactnativeWenderlich });
+class PropertyFinder extends React.Component {
+  render() {
+    return (
+      <ReactNative.Navigator
+        initialRoute={{ title: 'Property Finder', index: 0 }}
+        renderScene={(route, navigator) =>
+          <HelloWorld />
+        }
+        navigationBar={
+          <ReactNative.Navigator.NavigationBar
+            routeMapper={{
+              LeftButton: () => null,
+              RightButton: () => null,
+              Title: (route, navigator, index, navState) => <ReactNative.Text>Property Finder</ReactNative.Text>
+            }}
+            style={{backgroundColor: '#e0e0e0'}}
+          />
+        }
+        style={styles.container}
+      />
+    );
+  }
+}
+
+ReactNative.AppRegistry.registerComponent('tutorialReactnativeWenderlich', function() { return PropertyFinder });
