d4be5af18385ab702a4a680ce59609094abaa33f
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-12-25 17:21:24 -0800

    Styling with Flexbox (Closes #12)

--- SearchPage.js
+++ SearchPage.js
@@ -1,7 +1,6 @@
 'use strict';
 
 import React, { Component } from 'react';
-
 import {
     StyleSheet,
     Text,
@@ -23,6 +22,44 @@ var styles = StyleSheet.create({
         padding: 30,
         marginTop: 65,
         alignItems: 'center'
+    },
+    flowRight: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        alignSelf: 'stretch',
+        marginTop: 4,
+        marginBottom: 4
+    },
+    buttonText: {
+        fontSize: 18,
+        color: 'white',
+        alignSelf: 'center'
+    },
+    button: {
+        height: 36,
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: '#48BBEC',
+        borderColor: '#48BBEC',
+        borderWidth: 1,
+        borderRadius: 8,
+        alignSelf: 'stretch',
+        justifyContent: 'center'
+    },
+    searchInput: {
+        height: 36,
+        padding: 4,
+        marginRight: 8,
+        flex: 4,
+        fontSize: 16,
+        borderWidth: 1,
+        borderColor: '#48BBEC',
+        borderRadius: 8,
+        color: '#48BBEC'
+    },
+    image: {
+        width: 217,
+        height: 138
     }
 });
 
@@ -34,8 +71,23 @@ export default class SearchPage extends Component {
                     Search for houses to buy!
                 </Text>
                 <Text style={styles.description}>
-                    Search by place-name, postcode or search near your location.
+                    Search by place-name, zipcode or search near your location.
                 </Text>
+                <View style={styles.flowRight}>
+                    <TextInput style={styles.searchInput}
+                        placeholder='Search via name or zipcode'/>
+                    <TouchableHighlight style={styles.button}
+                        underlayColor='#99d9f4'>
+                        <Text style={styles.buttonText}>Go</Text>
+                    </TouchableHighlight>
+                </View>
+                <View style={styles.flowRight}>
+                    <TouchableHighlight style={styles.button}
+                        underlayColor='#99d9f4'>
+                        <Text style={styles.buttonText}>Location</Text>
+                    </TouchableHighlight>
+                </View>
+                <Image source={require('./resources/house.png')} style={styles.image} />
             </View>
         );
     }
